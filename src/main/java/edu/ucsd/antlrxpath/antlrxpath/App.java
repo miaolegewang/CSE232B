e94448b916abcf08f6e43c560711e5a0734c0d09
miaolegewang <jiansmith.zj@gmail.com>
2017-01-31 00:15:39 -0800

    Make test case available

--- src/main/java/edu/ucsd/antlrxpath/antlrxpath/App.java
+++ src/main/java/edu/ucsd/antlrxpath/antlrxpath/App.java
@@ -1,5 +1,11 @@
 package edu.ucsd.antlrxpath.antlrxpath;
 
+import java.util.ArrayList;
+
+import org.w3c.dom.Node;
+
+import edu.ucsd.xmlparser.DomParser;
+
 /**
  * Hello world!
  *
@@ -11,5 +17,14 @@ public class App
         /**
          * Now use manual test case input, later use file input
          */
+    	ArrayList<String> testcases = new ArrayList<String>();
+    	// Add test case here
+    	testcases.add("doc(\"j_caesar.xml\")//ACT/TITLE");
+    	
+    	XPathEval xquery = new XPathEval();
+    	for(int i = 0; i < testcases.size(); i++){
+    		ArrayList<Node> tmp = xquery.parse(testcases.get(i));
+    		DomParser.WriteXMLFile(tmp, "testcase_" + Integer.toString(i + 1) + ".xml");
+    	}
     }
 }
