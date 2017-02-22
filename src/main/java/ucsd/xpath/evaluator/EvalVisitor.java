82ae4197291517b971fc48a1927d514e5dcef6e6
miaolegewang <jiansmith.zj@gmail.com>
2017-02-22 03:46:35 -0800

    Not complete

--- src/main/java/ucsd/xpath/evaluator/EvalVisitor.java
+++ src/main/java/ucsd/xpath/evaluator/EvalVisitor.java
@@ -8,15 +8,11 @@ import org.antlr.v4.runtime.misc.NotNull;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
+import ucsd.xpath.evaluator.XQueryParser.BindContext;
+
 public class EvalVisitor extends XQueryBaseVisitor<List<Node>>{
-	HashMap<String, String> binding;
-	HashMap<String, List<Node>> store;
 	
-	/*
-	 * tempStore is used for temporary store for variables
-	 */
-	List<Node> tempStore;
-	
+	List<Node> temp = new ArrayList<Node>();
 	
 	/**
 	 * {@inheritDoc}
@@ -24,15 +20,24 @@ public class EvalVisitor extends XQueryBaseVisitor<List<Node>>{
 	 * <p>The default implementation returns the result of calling
 	 * {@link #visitChildren} on {@code ctx}.</p>
 	 */
-	@Override public List<Node> visitSl(@NotNull XQueryParser.SlContext ctx) {
-		List<Node> tmp = new ArrayList<Node>(tempStore);
-		tempStore.clear();
-		for(int i = 0; i < tmp.size(); i++){
-			NodeList children = tmp.get(i).getChildNodes();
-			for(int j = 0; j < children.getLength(); j++){
-				tempStore.add(children.item(j));
-			}
+	@Override public List<Node> visitFlwr(@NotNull XQueryParser.FlwrContext ctx) {
+		HashMap<String, Integer> level = new HashMap<String, Integer>();
+		List<Node> result = new ArrayList<Node>();
+		Integer counter = 0;
+		for(BindContext bind: ctx.forClause().bindings().bind()){
+			level.put(bind.var().varName().getText(), counter);
+			counter++;
 		}
-		return null;
+		List<Integer> pointers = new ArrayList<Integer>(0);
+		List<List<Node>> nodes = new ArrayList<List<Node>>();
+		
+		// Initialize each of the lists
+		nodes.add(visit(ctx.forClause().bindings().bind(0).query()));
+		for(int i = 1; i < counter; i++){
+			List<Node> old = new ArrayList<Node>(temp);
+			temp.clear();
+			temp.add(nodes[i - 1][0]);
+			nodes.add(visit())
+		}
 	}
 }
