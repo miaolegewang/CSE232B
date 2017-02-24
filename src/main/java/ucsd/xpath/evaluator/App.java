61199595dc5c4f8b93ae014047483b561c66eccb
Chen Lai <littlelittlecc@gmail.com>
2017-02-24 02:35:46 -0800

    handle file

--- src/main/java/ucsd/xpath/evaluator/App.java
+++ src/main/java/ucsd/xpath/evaluator/App.java
@@ -17,17 +17,23 @@ public class App
 {
     public static void main( String[] args )
     {
+//    	String filename = "integrationtest.txt";
+//    	String filename = "contest.txt";
+//    	String filename = "flwrtest.txt";
+//    	String filename = "consttest.txt";
+
+    	String filename = "test.txt";
     	
-    	List<String> testcases = Readfile.read("test.txt");
+    	List<String> testcases = Readfile.read(filename);
 
 
     	for(int i = 0; i < testcases.size(); i++){
     		XQueryEval xquery = new XQueryEval();
-    		System.out.println(testcases.get(i));
     		System.out.println("Start File " + Integer.toString(i));
+    		System.out.println(testcases.get(i));
     		List<Node> tmp = xquery.parse(testcases.get(i));
     		try{
-    			DomParser.WriteXMLFile(tmp, "testcase_" + Integer.toString(i + 1) + ".xml");
+    			DomParser.WriteXMLFile(tmp, filename + "_output_" + Integer.toString(i + 1) + ".xml");
     		}catch(Exception e){			
     			e.printStackTrace();
     		}
